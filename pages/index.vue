584937909286918fb503d64c9a205551db36232c
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2020-08-05 15:27:41 +0200

    trola2

--- pages/index.vue
+++ pages/index.vue
@@ -35,17 +35,10 @@
       <p class="mt-4">
         Created by
         <a
-          href="https://twitter.com/debs_obrien"
+          href="https://mikitvba.com"
           class="font-bold hover:underline"
-          >Debbie O'Brien</a
+          >Mihael Mutić</a
         >
-        at NuxtJS. See the
-        <a
-          href="https://nuxtjs.org/blog/creating-blog-with-nuxt-content"
-          class="font-bold hover:underline"
-          >tutorial</a
-        >
-        for how to build it.
       </p>
     </footer>
   </div>
